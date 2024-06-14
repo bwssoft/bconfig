a6ebdc682ed240c75e60c69d949d50dee998c434
devitalo97 <dev.italo.souza@gmail.com>
2024-06-14 15:45:52 -0300

    feat: add static method on 4g parser

--- app/lib/parser/4G.ts
+++ app/lib/parser/4G.ts
@@ -2,8 +2,27 @@ interface ParsedObject {
   [key: string]: string;
 }
 export class QuatroG {
+  static parse(input: string) {
+    if (
+      input.includes("Sim=") &&
+      input.includes("SOS=") &&
+      input.includes("APN=") &&
+      input.includes("TZ=") &&
+      input.includes("HB=")
+    ) {
+      return this.check(input)
+    } else if (
+      input.includes("BATTERY EXTERNAL:") &&
+      input.includes("BATT_INT:") &&
+      input.includes("ACC:")
+    ) {
+      return this.status(input)
+    } else {
+      return {}
+    }
+  }
 
-  check(input: string) {
+  static check(input: string) {
     const obj: ParsedObject = {};
     const regex = /(\w+[:=][^ ]+)/g;
     const matches = input.match(regex);
@@ -28,7 +47,7 @@ export class QuatroG {
     return obj;
   }
 
-  status(input: string) {
+  static status(input: string) {
     const obj: ParsedObject = {};
     const keyValuePairs = input.split(';');
 
@@ -48,9 +67,8 @@ export class QuatroG {
     return obj;
 
   }
-
 }
 
 // const check = "Sim=89883030000101192190 SOS= APN=bws.br,bws,bws TZ=W0 HB=60,1800 MG=0 TX=180 BJ=0 ACCMODE=1 TDET=0 WKMODE=0 DD=0 OD=0 ZD=7 AC=0,0 SDMS=2 TUR=1 PR=1 DK=1726 JD=48 LBS=* MODE=1 LED=1 IV=1 ACC=1 GPRS:4G E_UTRAN GPS:V PROT=E3+ DC:100,2000 Voltage:13.40,12.90 AF:OFF GS:80";
 
-// const statuss = 'BATTERY EXTERNAL:11.49V;BATT_INT:0%;ACC:ON;GPRS:Ok;GPS:0;GSM:20;HR: ;Buffer Memory:0;Tech:4G E_UTRAN;IP:143.198.247.1;Port:2000;ENGINE MODE1'
+// const status = "BATTERY EXTERNAL:11.49V;BATT_INT:0%;ACC:ON;GPRS:Ok;GPS:0;GSM:20;HR: ;Buffer Memory:0;Tech:4G E_UTRAN;IP:143.198.247.1;Port:2000;ENGINE MODE1"
