7b9d24ef6e370ae0a6c7f36e4be11e4a8e23f77d
devitalo97 <dev.italo.souza@gmail.com>
2024-06-16 22:53:38 -0300

    feat: use unified data from ports and implement function to open and close all ports

--- app/ui/pages/home/ports-feed.tsx
+++ app/ui/pages/home/ports-feed.tsx
@@ -9,17 +9,21 @@ const icons = {
   close: { icon: XMarkIcon, bgColorClass: "bg-gray-400" },
 };
 export default function PortsFeed(props: {
-  ports: { port: ISerialPort; open: boolean }[];
+  ports: {
+    port: ISerialPort;
+    open: boolean;
+    reader?: ReadableStreamDefaultReader<Uint8Array> | undefined;
+    info?: { usbProductId?: number; usbVendorId?: number };
+    imei?: string;
+    iccid?: string;
+    et?: string;
+  }[];
   openPort: (port: ISerialPort) => Promise<void>;
   closePort: (port: ISerialPort) => Promise<void>;
   requestPort: () => Promise<void>;
   forgetPort: (port: ISerialPort) => Promise<void>;
 }) {
   const { ports, openPort, closePort, requestPort, forgetPort } = props;
-  const _ports = ports.map((p) => ({
-    info: p.port.getInfo(),
-    ...p,
-  }));
 
   const handleOpenPort = async (port: ISerialPort) => {
     if (port.writable && port.readable) {
@@ -33,16 +37,23 @@ export default function PortsFeed(props: {
     await forgetPort(port);
   };
 
+  const handleOpenAllPorts = async () => {
+    for (let p of ports) {
+      await handleOpenPort(p.port);
+    }
+  };
+
   return (
     <>
       <div className="mt-6 flow-root">
-        {_ports.length > 0 ? (
+        {ports.length > 0 ? (
           <ul role="list" className="-mb-8">
-            {_ports.map((item, itemIdx) => {
-              const { info, port, open } = item;
+            {ports.map((item, itemIdx) => {
+              const { info, port, open, imei } = item;
               const isOpen = open;
+              const random = Math.random();
               return (
-                <li key={info.usbProductId}>
+                <li key={random}>
                   <div className="relative pb-8">
                     {itemIdx !== ports.length - 1 ? (
                       <span
@@ -73,9 +84,9 @@ export default function PortsFeed(props: {
                       </span>
                       <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5 items-center">
                         <p className="text-sm text-gray-500">
-                          {info.usbProductId}{" "}
+                          {info?.usbProductId}
                           <a href="#" className="font-medium text-gray-900">
-                            {info.usbVendorId}
+                            {imei ?? info?.usbVendorId}
                           </a>
                         </p>
                         <div className="flex whitespace-nowrap text-right text-sm text-gray-500 gap-2">
@@ -107,13 +118,27 @@ export default function PortsFeed(props: {
           <Alert label="Você não tem nenhuma porta registrada. Aberte o botão abaixo e escolha uma." />
         )}
       </div>
-      <div className="mt-6 flex flex-col justify-stretch">
+      <div className="mt-6 flex flex-col justify-stretch gap-3">
         <button
           type="button"
           className="inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           onClick={requestPort}
         >
           Requisitar nova porta
+        </button>
+        <button
+          type="button"
+          className="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={handleOpenAllPorts}
+        >
+          Abrir todas as portas
+        </button>
+        <button
+          type="button"
+          className="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={handleOpenAllPorts}
+        >
+          Fechar todas as portas
         </button>
       </div>
     </>
