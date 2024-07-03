d32d58dbdba3860af14592d06cfba29bbe539eb9
devitalo97 <dev.italo.souza@gmail.com>
2024-07-03 10:42:34 -0300

    refactor(hook): use Promise.all instead for loop

--- app/hook/use-e3-communication.ts
+++ app/hook/use-e3-communication.ts
@@ -1,46 +1,53 @@
-import { useEffect, useState, useRef } from "react";
-import { useSerial } from "./use-serial";
-import { ISerialPort } from "../lib/definitions/serial";
-import { sleep } from "../lib/util/sleep";
-import { E3 } from "../lib/parser/e3+";
+import { useEffect, useState, useRef } from "react"
+import { useSerial } from "./use-serial"
+import { ISerialPort } from "../lib/definitions/serial"
+import { sleep } from "../lib/util/sleep"
+import { E3 } from "../lib/parser/e3+"
 
 type Identified = { port: ISerialPort, imei?: string, check?: string, iccid?: string }
-type Data = { port: ISerialPort, message: string, timestamp: number }
 
 export function useE3Communication() {
   const { ports, writeToPort, openPort, getReader, requestPort } = useSerial({
-    handleConnection: () => { },
     handleDisconnection: (port) => {
-      setIdentified(prev => prev.filter(el => el.port !== port));
-      setLog(prev => prev.filter(el => el.port !== port));
-    },
-  });
-  const [identified, setIdentified] = useState<Identified[]>([]);
-  const [log, setLog] = useState<Data[]>([])
+      setIdentified(prev => prev.filter(el => el.port !== port))
+    }
+  })
+  const [identified, setIdentified] = useState<Identified[]>([])
 
-  const indentifierDevices = async (ports: ISerialPort[]) => {
-    for (let port of ports) {
+  const getDeviceInfo = async (ports: ISerialPort[]) => {
+    await Promise.all(ports.map(async port => {
       if (!port.readable && !port.writable) {
         let reader: ReadableStreamDefaultReader<Uint8Array> | undefined
         try {
-          await openPort(port);
-          reader = await getReader(port);
+          await openPort(port)
+          reader = await getReader(port)
           if (typeof reader !== "undefined") {
             const _reader = reader
-            await writeToPort(port, "REG000000#")
-            await sleep(500)
-            await writeToPort(port, "SMS1")
-            await sleep(500)
-            await writeToPort(port, "EN")
-            await sleep(500)
-            await writeToPort(port, "IMEI")
-            const imei = await readUntilCommand(_reader, "IMEI")
-            await sleep(500)
-            await writeToPort(port, "ICCID")
-            const iccid = await readUntilCommand(_reader, "ICCID")
-            await sleep(500)
-            await writeToPort(port, "CHECK")
-            const check = await readUntilCommand(_reader, "CHECK")
+            let imei, iccid, check
+            while (!imei || !iccid || !check) {
+              await writeToPort(port, "REG000000#")
+              await sleep(500)
+              await writeToPort(port, "SMS1")
+              await sleep(500)
+              await writeToPort(port, "EN")
+              await sleep(500)
+
+              if (!imei) {
+                await writeToPort(port, "IMEI")
+                imei = await readDeviceResponse(_reader, "IMEI")
+              }
+              await sleep(500)
+              if (!iccid) {
+                await writeToPort(port, "ICCID")
+                iccid = await readDeviceResponse(_reader, "ICCID")
+              }
+              await sleep(500)
+              if (!check) {
+                await writeToPort(port, "CHECK")
+                check = await readDeviceResponse(_reader, "CHECK")
+              }
+              await sleep(500)
+            }
             setIdentified(prev => [
               ...prev,
               {
@@ -55,31 +62,32 @@ export function useE3Communication() {
           console.error("error on init", e)
           reader?.releaseLock()
           await port.close()
-          await indentifierDevices([port])
+          await getDeviceInfo([port])
         }
       }
-    }
+    }))
   }
-  const readUntilCommand = async (reader: ReadableStreamDefaultReader<Uint8Array>, command: string) => {
-    const decoder = new TextDecoder();
-    let buffer = "";
-    let foundCommandResponse = false;
+
+  const readDeviceResponse = async (reader: ReadableStreamDefaultReader<Uint8Array>, command: string) => {
+    const decoder = new TextDecoder()
+    let buffer = ""
+    let foundCommandResponse = false
     while (true) {
-      const { value, done } = await reader.read();
-      if (done) break;
+      const { value, done } = await reader.read()
+      if (done) break
 
-      const chunk = decoder.decode(value, { stream: true });
-      buffer += chunk;
+      const chunk = decoder.decode(value, { stream: true })
+      buffer += chunk
 
-      let lines = buffer.split("\r\n");
-      buffer = lines.pop() || "";
+      let lines = buffer.split("\r\n")
+      buffer = lines.pop() || ""
       for (const line of lines) {
         if (line.length > 0) {
-          const timestamp = Date.now();
+          const timestamp = Date.now()
           if (line.includes(`SMS:${command}`)) {
-            foundCommandResponse = true;
+            foundCommandResponse = true
           } else if (foundCommandResponse) {
-            return line;
+            return line
           }
         }
       }
@@ -87,8 +95,8 @@ export function useE3Communication() {
   }
 
   useEffect(() => {
-    indentifierDevices(ports)
-  }, [ports]);
+    getDeviceInfo(ports)
+  }, [ports])
 
-  return { log, identified, requestPort, ports };
+  return { identified, requestPort, ports }
 }
