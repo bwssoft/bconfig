a586255c051db9d7f592315a504b0a5c28e34bea
devitalo97 <dev.italo.souza@gmail.com>
2024-07-03 23:56:22 -0300

    feat(hook): request more data from serial port

--- app/hook/use-e3-communication.ts
+++ app/hook/use-e3-communication.ts
@@ -4,7 +4,20 @@ import { ISerialPort } from "../lib/definitions/serial"
 import { sleep } from "../lib/util/sleep"
 import { E3 } from "../lib/parser/e3+"
 
-type Identified = { port: ISerialPort, imei?: string, check?: string, iccid?: string }
+type Identified = {
+  port: ISerialPort,
+  imei?: string,
+  iccid?: string,
+  initialConfig?: {
+    check?: string
+    cxip?: string
+    et?: string
+    status?: string
+  }
+  config?: {
+    ip: any
+  }
+}
 
 export function useE3Communication() {
   const { ports, writeToPort, openPort, getReader, requestPort } = useSerial({
@@ -23,8 +36,9 @@ export function useE3Communication() {
           reader = await getReader(port)
           if (typeof reader !== "undefined") {
             const _reader = reader
-            let imei, iccid, check
-            while (!imei || !iccid || !check) {
+            let imei, iccid, check, cxip, dns;
+            let et, status
+            while (!imei || !iccid || !check || !cxip || !dns || !et || !status) {
               await writeToPort(port, "REG000000#")
               await sleep(500)
               await writeToPort(port, "SMS1")
@@ -47,16 +61,56 @@ export function useE3Communication() {
                 check = await readDeviceResponse(_reader, "CHECK")
               }
               await sleep(500)
-            }
-            setIdentified(prev => [
-              ...prev,
-              {
-                port,
-                iccid: iccid ? E3.iccid(iccid) : undefined,
-                imei: imei ? E3.imei(imei) : undefined,
-                check
+              if (!cxip) {
+                await writeToPort(port, "CXIP")
+                cxip = await readDeviceResponse(_reader, "CXIP")
               }
-            ])
+              await sleep(500)
+              if (!dns) {
+                await writeToPort(port, "DNS")
+                dns = await readDeviceResponse(_reader, "DNS")
+              }
+              await sleep(500)
+              if (!et) {
+                await writeToPort(port, "ET")
+                et = await readDeviceResponse(_reader, "ET")
+              }
+              await sleep(500)
+              if (!status) {
+                await writeToPort(port, "STATUS")
+                status = await readDeviceResponse(_reader, "STATUS")
+              }
+            }
+            setIdentified(prev => {
+              const _check = check ? E3.check(check) : undefined
+              console.log('check', check)
+              return [
+                ...prev,
+                {
+                  port,
+                  iccid: iccid ? E3.iccid(iccid) : undefined,
+                  imei: imei ? E3.imei(imei) : undefined,
+                  initialConfig: {
+                    check: check ?? undefined,
+                    cxip: cxip ?? undefined,
+                    et: et ?? undefined,
+                    status: status ?? undefined,
+                  },
+                  config: {
+                    ip: cxip ? E3.ip(cxip) : undefined,
+                    dns: dns ? E3.dns(dns) : undefined,
+                    apn: _check?.apn ?? undefined,
+                    timezone: _check?.timezone ?? undefined,
+                    lock_type: _check?.lock_type ?? undefined,
+                    data_transmission: _check?.data_transmission ?? undefined,
+                    odometer: _check?.odometer ?? undefined,
+                    keep_alive: _check?.keep_alive ?? undefined,
+                    accelerometer_sensitivity: _check?.accelerometer_sensitivity ?? undefined,
+                    economy_mode: _check?.economy_mode ?? undefined
+                  },
+                }
+              ]
+            })
           }
         } catch (e) {
           console.error("error on init", e)
--- app/hook/use-serial.ts
+++ app/hook/use-serial.ts
@@ -12,38 +12,38 @@ export const useSerial = (props: Props) => {
   const isFirstRendering = useRef(true);  // Referência para controlar a primeira execução
 
   useEffect(() => {
-    if (isFirstRendering.current) {
-      isFirstRendering.current = false
-      if (typeof window !== "undefined" && "serial" in navigator) {
-        const _navigator = navigator as INavigator
+    // if (isFirstRendering.current) {
+    //   isFirstRendering.current = false
+    if (typeof window !== "undefined" && "serial" in navigator) {
+      const _navigator = navigator as INavigator
 
-        _navigator.serial.getPorts().then(async (ports) => {
-          setPorts(ports);
-        });
+      _navigator.serial.getPorts().then(async (ports) => {
+        setPorts(ports);
+      });
 
-        const handleConnect = (e: Event) => {
-          const target = e.target as ISerialPort | null
-          if (!target) return
-          setPorts((prev) => [...prev, target])
-          handleConnection?.(target)
-        };
+      const handleConnect = (e: Event) => {
+        const target = e.target as ISerialPort | null
+        if (!target) return
+        setPorts((prev) => [...prev, target])
+        handleConnection?.(target)
+      };
 
-        const handleDisconnect = (e: Event) => {
-          const target = e.target as ISerialPort | null
-          if (!target) return
-          setPorts((prev) => prev.filter(el => el !== target))
-          handleDisconnection?.(target)
-        };
+      const handleDisconnect = (e: Event) => {
+        const target = e.target as ISerialPort | null
+        if (!target) return
+        setPorts((prev) => prev.filter(el => el !== target))
+        handleDisconnection?.(target)
+      };
 
-        _navigator.serial.addEventListener("connect", handleConnect);
-        _navigator.serial.addEventListener("disconnect", handleDisconnect);
+      _navigator.serial.addEventListener("connect", handleConnect);
+      _navigator.serial.addEventListener("disconnect", handleDisconnect);
 
-        return () => {
-          _navigator.serial.removeEventListener("connect", handleConnect);
-          _navigator.serial.removeEventListener("disconnect", handleDisconnect);
-        };
-      }
+      return () => {
+        _navigator.serial.removeEventListener("connect", handleConnect);
+        _navigator.serial.removeEventListener("disconnect", handleDisconnect);
+      };
     }
+    // }
   }, []);
 
   //Serial
