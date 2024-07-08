b72471dae8c0bb287cc1e84a04bdca15e8c729c7
devitalo97 <dev.italo.souza@gmail.com>
2024-07-08 16:11:40 -0300

    refactor(page): modify ui to handle new data from hook

--- app/configurator/components/panel.tsx
+++ app/configurator/components/panel.tsx
@@ -3,9 +3,8 @@
 import { IProfile } from "@/app/lib/definition";
 import Alert from "@/app/ui/components/alert";
 import { Button } from "@/app/ui/components/button";
-import ConfigurationTable from "@/app/ui/tables/configuration-log/table";
+import DeviceConfiguredTable from "@/app/ui/tables/devices-configured/table";
 import DevicesToConfigureTable from "@/app/ui/tables/devices-to-configure/table";
-import CommandLogTable from "@/app/ui/tables/command-log/table";
 import { useE3Communication } from "@/app/hook/use-e3-communication";
 
 interface Props {
@@ -71,34 +70,18 @@ export function Panel(props: Props) {
           </div>
         </div>
       </div>
-      {configurations.length ? (
-        <div className="mt-10 flex flex-col gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
-          <div>
-            <h1 className="text-base font-semibold leading-7 text-gray-900">
-              Etapa 3: Verificação
-            </h1>
-            <p className="mt-2 text-sm text-gray-700">
-              Uma lista de todos os equipamentos configurados e os comandos
-              enviados.
-            </p>
-          </div>
-          <div className="flex flex-col gap-6 w-full">
-            <ConfigurationTable data={configurations} />
-            {/* {configurations.map((i, idx) => {
-              return (
-                <div key={idx}>
-                  <h1 className="text-sm font-semibold leading-7 text-gray-900">
-                    Imei: {i.imei}
-                  </h1>
-                  <CommandLogTable data={i.metadata.commands_sent} />
-                </div>
-              );
-            })} */}
-          </div>
+      <div className="mt-10 flex flex-col gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
+        <div>
+          <h1 className="text-base font-semibold leading-7 text-gray-900">
+            Etapa 3: Verificação
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            Uma lista de todos os equipamentos configurados e os comandos
+            enviados.
+          </p>
         </div>
-      ) : (
-        <></>
-      )}
+        <DeviceConfiguredTable data={configurations} />
+      </div>
     </>
   );
 }
