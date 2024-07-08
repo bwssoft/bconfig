159a4b01065ff17fad03795517084a931d92b7fe
devitalo97 <dev.italo.souza@gmail.com>
2024-07-08 16:08:12 -0300

    feat(ui/form): set empty string and 0 value into undefined and remove undefined  properties from schema

--- app/ui/forms/use-profile.form.tsx
+++ app/ui/forms/use-profile.form.tsx
@@ -1,71 +1,78 @@
-import { economyMode } from "@/app/constants/e3+config";
 import { createOneProfile } from "@/app/lib/action";
-import { E3Encoder } from "@/app/lib/encoder/e3+";
+import { removeUndefined } from "@/app/lib/util";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const schema = z.object({
-  name: z.string(),
-  model: z.enum(["E3+", "E3+4G"]),
-  password: z.object({ old: z.string(), new: z.string() }).optional(),
-  apn: z
-    .object({
-      address: z.string(),
-      user: z.string(),
-      password: z.string(),
-    })
-    .optional(),
-  ip: z
-    .object({
-      primary: z.object({
-        ip: z.string(),
-        port: z.string(),
-      }),
-      secondary: z.object({
-        ip: z.string(),
-        port: z.string(),
-      }),
-    })
-    .optional(),
-  dns: z
-    .object({
-      address: z.string(),
-      port: z.string(),
-    })
-    .optional(),
-  timezone: z.coerce.number().optional(),
-  lock_type: z.coerce.number().optional(),
-  data_transmission: z
-    .object({
-      on: z.string(),
-      off: z.string(),
-    })
-    .optional(),
-  odometer: z.coerce.number().optional(),
-  keep_alive: z.coerce.number().optional(),
-  accelerometer_sensitivity: z.coerce.number().optional(),
-  economy_mode: z.coerce.number().optional(),
-  sensitivity_adjustment: z.coerce.number().optional(),
-  lbs_position: z.coerce.boolean().optional().default(false),
-  cornering_position_update: z.coerce.boolean().optional().default(false),
-  ignition_alert_power_cut: z.coerce.boolean().optional().default(false),
-  gprs_failure_alert: z.coerce.boolean().optional().default(false),
-  led: z.coerce.boolean().optional().default(false),
-  virtual_ignition: z.coerce.boolean().optional().default(false),
-  // panic_button: z.coerce.boolean().optional().default(false),
-  // module_violation: z.coerce.boolean().optional().default(false),
-});
+const numberTransform = (number?: number) =>
+  number === 0 ? undefined : number;
+
+const stringTransform = (str?: string) => (str === "" ? undefined : str);
+
+const zodNumber = z.coerce.number().optional().transform(numberTransform);
+const zodString = z.string().optional().transform(stringTransform);
+const schema = z
+  .object({
+    name: z.string(),
+    model: z.enum(["E3+", "E3+4G"]),
+    password: z.object({ old: zodString, new: zodString }).optional(),
+    apn: z
+      .object({
+        address: zodString,
+        user: zodString,
+        password: zodString,
+      })
+      .optional(),
+    ip: z
+      .object({
+        primary: z.object({
+          ip: zodString,
+          port: zodString,
+        }),
+        secondary: z.object({
+          ip: zodString,
+          port: zodString,
+        }),
+      })
+      .optional(),
+    dns: z
+      .object({
+        address: zodString,
+        port: zodString,
+      })
+      .optional(),
+    timezone: zodNumber,
+    lock_type: zodNumber,
+    data_transmission: z
+      .object({
+        on: zodString,
+        off: zodString,
+      })
+      .optional(),
+    odometer: zodNumber,
+    keep_alive: zodNumber,
+    accelerometer_sensitivity: zodNumber,
+    economy_mode: zodNumber,
+    sensitivity_adjustment: zodNumber,
+    lbs_position: z.coerce.boolean().optional().default(false),
+    cornering_position_update: z.coerce.boolean().optional().default(false),
+    ignition_alert_power_cut: z.coerce.boolean().optional().default(false),
+    gprs_failure_alert: z.coerce.boolean().optional().default(false),
+    led: z.coerce.boolean().optional().default(false),
+    virtual_ignition: z.coerce.boolean().optional().default(false),
+    // panic_button: z.coerce.boolean().optional().default(false),
+    // module_violation: z.coerce.boolean().optional().default(false),
+  })
+  .transform((value) => removeUndefined(value));
 
 export type Schema = z.infer<typeof schema>;
 
 interface Props {
   defaultValues?: Schema;
-  onSubmit?: (commands: string[]) => Promise<void>;
 }
 export function useProfileCreateForm(props: Props) {
-  const { defaultValues, onSubmit } = props;
+  const { defaultValues } = props;
   const {
     register,
     handleSubmit: hookFormSubmit,
