7509ed3dc1e809baf715bd69be73640ee79f7e78
devitalo97 <dev.italo.souza@gmail.com>
2024-08-09 12:58:47 -0300

    style: add logo on sidebar and login page

--- app/(guest)/login/page.tsx
+++ app/(guest)/login/page.tsx
@@ -28,7 +28,7 @@ export default function Example() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             alt="Your Company"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src="/logo-bws.png"
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
--- app/ui/components/side-bar.tsx
+++ app/ui/components/side-bar.tsx
@@ -214,7 +214,7 @@ export function SideBar(props: Props) {
                     <div className="flex h-16 shrink-0 items-center">
                       <img
                         className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        src="/logo-bws.png"
                         alt="Your Company"
                       />
                     </div>
@@ -241,7 +241,7 @@ export function SideBar(props: Props) {
             <div className="flex h-16 shrink-0 items-center">
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src="/logo-bws.png"
                 alt="Your Company"
               />
             </div>
