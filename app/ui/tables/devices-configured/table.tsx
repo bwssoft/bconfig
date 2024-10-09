2a25473f35cb4871c010d8101604e34cdd683d8b
devitalo97 <dev.italo.souza@gmail.com>
2024-10-09 15:04:03 -0300

    fix: build

--- app/ui/tables/devices-configured/table.tsx
+++ app/ui/tables/devices-configured/table.tsx
@@ -14,6 +14,7 @@ interface Props {
     is_configured: boolean;
     not_configured: any;
     profile_name: string;
+    model: string;
   }[];
 }
 export default function DeviceConfiguredTable(props: Props) {
