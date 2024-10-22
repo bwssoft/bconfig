c6bca7bb926ee3d5fbeee7f3c573f3a77d9ed4e0
devitalo97 <dev.italo.souza@gmail.com>
2024-10-22 14:18:56 -0300

    refactor(configurator/e3+): modify file name

---
+++ app/(client)/configuration/page.tsx
@@ -0,0 +1,38 @@
+import { findManyByModel, findOneProfile } from "@/app/lib/action";
+import { IProfile } from "@/app/lib/definition";
+
+interface Props {
+  searchParams: {
+    id: string;
+  };
+}
+
+export default async function Page(props: Props) {
+  const {
+    searchParams: { id },
+  } = props;
+  const profiles = await findManyByModel("E3+4G" as IProfile["model"]);
+  const profileSelected = (await findOneProfile({ id })) ?? undefined;
+  const date = new Date();
+  return (
+    <div>
+      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8 ">
+        <div>
+          <h1 className="text-base font-semibold leading-7 text-gray-900">
+            Configurador
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            Ferramenta para configurar a{" "}
+            <a href="#" className="text-gray-900">
+              Família E3+4G.
+            </a>{" "}
+            Data de hoje:{" "}
+            <time dateTime={date.toLocaleDateString()}>
+              {date.toLocaleDateString()}
+            </time>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
