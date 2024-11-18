1ca4065926cd509dd59add2c959467fc56aff738
Lauane Oliveira <oliveiralauane91@gmail.com>
2024-11-18 10:59:33 -0300

    fix: change label in N1 and N2 fields

--- app/ui/forms/profile/update/e34g/update-e34g-profile.form.tsx
+++ app/ui/forms/profile/update/e34g/update-e34g-profile.form.tsx
@@ -288,7 +288,7 @@ export function E34GProfileUpdateForm(props: Props) {
                     <Input
                       {...register("lock_type_progression.n1")}
                       id="lock_type_progression_n1"
-                      label="N1"
+                      label="Tempo acionado"
                       placeholder="60"
                       type="number"
                       error={errors.lock_type_progression?.n1?.message}
@@ -296,7 +296,7 @@ export function E34GProfileUpdateForm(props: Props) {
                     <Input
                       {...register("lock_type_progression.n2")}
                       id="lock_type_progression_n2"
-                      label="n2"
+                      label="Tempo não acionado"
                       placeholder="180"
                       type="number"
                       error={errors.lock_type_progression?.n2?.message}
