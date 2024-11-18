2f4b9cf75b1e0be855f9f601e24e5aa12c368e1a
Lauane Oliveira <oliveiralauane91@gmail.com>
2024-11-18 15:57:58 -0300

    feat: add create user form

--- app/(user)/user/page.tsx
+++ app/(user)/user/page.tsx
@@ -23,7 +23,7 @@ export default async function Example() {
         <div className="ml-auto flex gap-6">
             {session?.user?.type === "employee" && (
                 <Link
-                    href="/profile/form/create/e3+4g"
+                    href="/user/form/create/"
                     className="flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                     <PlusIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
--- app/lib/action/user.action.ts
+++ app/lib/action/user.action.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { auth } from "@/auth";
 import { IUser } from "../definition";
 import userRepository from "../repository/mongodb/user.repository"
 import { revalidatePath } from "next/cache"
@@ -14,6 +15,25 @@ export async function findOneUser(props: { email: string, connected?: boolean })
 export async function findAllUsers(input?: Partial<IUser>): Promise<IUser[]> {
   return await repository.findMany(input ?? {}) as IUser[]
 }
+
+export async function createOneUser(input: Omit<IUser
+  , "id" | "created_at" | "user_id">) {
+  const session = await auth()
+  const created_at = new Date()
+  const id = crypto.randomUUID()
+  const _input = {
+    ...input,
+    created_at,
+    id,
+    user_id: session?.user.id!
+  }
+  await repository.create(_input)
+  revalidatePath("/user")
+  revalidatePath("/configurator")
+  revalidatePath("/configuration")
+  return _input
+}
+
 
 export async function updateOneUserById(query: { id: string }, value: Omit<IUser, "id" | "created_at">) {
   const result = await repository.updateOne(query, value)
