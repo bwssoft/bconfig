169c5b710d9189e4c92490e789cb375f900db5a1
diego-dps <diego.pereira2806@hotmail.com>
2024-11-19 09:26:49 -0300

    feat: adjust update toggles

--- app/ui/forms/profile/update/e3/update-e3-profile.form.tsx
+++ app/ui/forms/profile/update/e3/update-e3-profile.form.tsx
@@ -412,14 +412,6 @@ export function E3ProfileUpdateForm(props: Props) {
             <div className="divide-y divide-gray-200 border-b border-t border-gray-200">
               {functions.map((func, id) => (
                 <div key={id} className="relative flex items-center py-4">
-                  <div className="min-w-0 flex-1 text-sm leading-6">
-                    <label
-                      htmlFor={`functions-${func.name}`}
-                      className="select-none font-medium text-gray-900"
-                    >
-                      {func.label}
-                    </label>
-                  </div>
                   <div className="ml-3 flex h-6 items-center gap-2">
                     <Controller
                       control={control}
@@ -429,6 +421,15 @@ export function E3ProfileUpdateForm(props: Props) {
                       )}
                     />
                   </div>
+                  <div className="min-w-0 flex-1 text-sm leading-6 ml-4">
+                    <label
+                      htmlFor={`functions-${func.name}`}
+                      className="select-none font-medium text-gray-900"
+                    >
+                      {func.label}
+                    </label>
+                  </div>
+                  
                 </div>
               ))}
             </div>
@@ -476,14 +477,6 @@ export function E3ProfileUpdateForm(props: Props) {
                       </label>
                     </div>
                   </div>
-                  <div className="ml-3 min-w-0 flex-1 text-sm leading-6">
-                    <label
-                      htmlFor={`functions-${func.name}`}
-                      className="select-none font-medium text-gray-900"
-                    >
-                      {func.label}
-                    </label>
-                  </div>
                   <div className="ml-3 flex h-6 items-center gap-2">
                     <Controller
                       control={control}
@@ -493,6 +486,15 @@ export function E3ProfileUpdateForm(props: Props) {
                       )}
                     />
                   </div>
+                  <div className="ml-3 min-w-0 flex-1 text-sm leading-6">
+                    <label
+                      htmlFor={`functions-${func.name}`}
+                      className="select-none font-medium text-gray-900"
+                    >
+                      {func.label}
+                    </label>
+                  </div>
+                  
                 </div>
               ))}
             </div>
